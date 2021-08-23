03c7123bf50a083259a480eddd84fcae54cbcc63
codefingas <udohezekiel34@yahoo.com>
2021-08-23 11:15:51 +0100

    update: set up express and graphQL

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import { graphqlHTTP } from "express-graphql";
 
 const app = express();
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
+
+app.use('/graphql', graphqlHTTP({
+    //will pass option later on
+}));
+
 
 app.listen(port, () => {
     console.log(`now listening for requests on - ${port}`)
